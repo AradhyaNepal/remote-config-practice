2c3a928359a13b986e97b2fa1313b9b36a643246
Aradhya Nepal <aradhya.1221@gmail.com>
2024-01-04 12:59:47 +0545

    Hive already intialized blocker setup

--- lib/common/repository/base_local_repo.dart
+++ lib/common/repository/base_local_repo.dart
@@ -4,14 +4,22 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:remote_config_practice/common/repository/user_preferences_local_repo.dart';
 
 abstract class BaseLocalRepo {
+  //Todo: Better alternative from the Hive class itself would be better option
+  static bool _hiveLocationInitialized = false;
+
   static Future<void> initAll() async {
     await _initializeHiveLocation();
     await UserPreferencesLocalRepo().init();
   }
 
   static Future<void> _initializeHiveLocation() async {
+    if (_hiveLocationInitialized) {
+      log("Hive already initialized");
+      return;
+    }
     try {
       await Hive.initFlutter();
+      _hiveLocationInitialized = true;
     } catch (e, s) {
       log(e.toString());
       log(s.toString());
@@ -33,7 +41,7 @@ abstract class BaseLocalRepo {
     }
   }
 
-  Future<void> put<E>(String key, E value) async {
+  Future<void> put(String key, dynamic value) async {
     await _hiveBox.put(key, value);
   }
 
